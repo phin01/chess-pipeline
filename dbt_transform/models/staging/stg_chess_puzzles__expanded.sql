d02e3bd4b38d17fdb754f6942e63abd9a6f1a5f5
Paulo Nascimento <phin01@gmail.com>
2026-05-01 15:00:21 -0300

    fixes to staging expanded table and macro for time conversion in seconds

--- dbt_transform/models/staging/stg_chess_puzzles__expanded.sql
+++ dbt_transform/models/staging/stg_chess_puzzles__expanded.sql
@@ -1,9 +1,9 @@
 select 
     puzzle_id,
-    payload.average_time,
+    {{ time_to_seconds('JSON_VALUE(payload, "$.average_time")') }} as average_time,
     payload.code,
     payload.correct_move_count,
-    payload.date,
+    TIMESTAMP(JSON_VALUE(payload, '$.date')) as date_utc,
     payload.fen,
     payload.flip_board,
     payload.is_passed,
@@ -20,6 +20,3 @@ select
     ingested_at
 
 from {{ ref('stg_chess_puzzles__payload') }}
-
-where 1=1
-and puzzle_id in ('987224', '1552226')
