220c5f4c0462d76ea791b5b8f542f4726712f637
Paulo Nascimento <phin01@gmail.com>
2026-05-02 15:17:37 -0300

    update d_tags to include 'No Tag' entry

--- dbt_transform/models/marts/d_tags.sql
+++ dbt_transform/models/marts/d_tags.sql
@@ -1,7 +1,4 @@
 SELECT DISTINCT
-  TRIM(tag) AS tag_name
+  puzzle_tag AS tag_name
 
-FROM {{ ref('int_chess_puzzles') }},
-
-UNNEST(SPLIT(labeled_tags, ',')) AS tag
-WHERE tag IS NOT NULL AND tag != ''
+FROM {{ ref('link_puzzle_tag') }}
