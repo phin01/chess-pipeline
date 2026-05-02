dce3394d9fea8a266d73f129d5e7b47315b5eabd
Paulo Nascimento <phin01@gmail.com>
2026-05-02 11:15:41 -0300

    update int puzzles model with new_rating_system column and docs

--- dbt_transform/models/intermediate/int_chess_puzzles.sql
+++ dbt_transform/models/intermediate/int_chess_puzzles.sql
@@ -8,6 +8,10 @@ select
     labeled_tags,
     rating as puzzle_rating,
     target_time,
+    CASE
+        WHEN date_utc <= '2025-10-08' THEN false
+        ELSE true
+    END AS new_rating_system,
 
     -- puzzle attempt specific info
     date_utc,
